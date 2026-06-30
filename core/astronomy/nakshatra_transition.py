26bf8ea635337cefa6d61e24d5e9364ab02f5559
kamalnathvg <kamalnath7171@gmail.com>
2026-06-30 12:43:28 -0400

    changed nakshatra transition end_time

--- core/astronomy/nakshatra_transition.py
+++ core/astronomy/nakshatra_transition.py
@@ -23,7 +23,7 @@ class NakshatraTransition(BaseModel):
     name: str
     nakshatra: Nakshatra
     start_time: datetime
-    end_time: datetime 
+    end_time: datetime | None
 
     @field_serializer('nakshatra')
     def ser_nakshatra(self, n: Nakshatra):
